305694c6966d8d6abcaafa171b9df78e54d3943a
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-06-02 18:39:34 +0530

    - Create `FavoriteButton` with binding variable for change value and update the view

---
+++ Landmarks-SwiftUI/Views/FavoriteButton.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteButton.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Hitesh on 02/06/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggel Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(.yellow)
+        }
+    }
+}
+
+/*
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(false))
+    }
+}
+*/
