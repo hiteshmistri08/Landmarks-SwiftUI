a7b4bbe1c01cb5cf830f678e85cf5b186ddc6b59
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-06-09 10:42:13 +0530

    - Add RotatedBadgeSymbol

---
+++ Landmarks-SwiftUI/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,26 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Hitesh on 09/06/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+/*
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
+*/
