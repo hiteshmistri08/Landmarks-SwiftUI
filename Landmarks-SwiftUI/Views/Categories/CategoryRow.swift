f9e272a31e9e8c04e4a15940ec46f791e478bb3c
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-16 20:57:43 +0530

    - Add CategoryItem in Category Row

--- Landmarks-SwiftUI/Views/Categories/CategoryRow.swift
+++ Landmarks-SwiftUI/Views/Categories/CategoryRow.swift
@@ -12,7 +12,7 @@ struct CategoryRow: View {
     var items: [Landmark]
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text(categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
@@ -21,7 +21,7 @@ struct CategoryRow: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        Text(landmark.name)
+                        CategoryItem(landmark: landmark)
                     }
                 }
             }
