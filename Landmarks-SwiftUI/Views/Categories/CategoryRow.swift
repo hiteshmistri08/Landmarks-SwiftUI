2bd8f9fa547b743d632e25ac38ec26c972142b71
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-16 20:48:52 +0530

    - Create CategoryRow

---
+++ Landmarks-SwiftUI/Views/Categories/CategoryRow.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryRow.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Hitesh on 16/07/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        Text(landmark.name)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        let model = ModelData().landmarks
+        CategoryRow(categoryName: model[0].category.rawValue, items: Array(model.prefix(4)))
+    }
+}
