dc5902e1a019fbbb94ae0f423c13658e7ee733d3
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-16 20:54:55 +0530

    - Create CategoryItem

---
+++ Landmarks-SwiftUI/Views/Categories/CategoryItem.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryItem.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Hitesh on 16/07/24.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height:155)
+                .cornerRadius(5)
+            
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        let model = ModelData().landmarks[0]
+        CategoryItem(landmark: model)
+    }
+}
