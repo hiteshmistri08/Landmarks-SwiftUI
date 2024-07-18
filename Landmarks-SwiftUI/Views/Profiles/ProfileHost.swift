5fe6c13d1bc453519e0a900d21f10095385a90de
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-18 20:36:43 +0530

    - Create ProfileHost

---
+++ Landmarks-SwiftUI/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHost.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Hitesh on 18/07/24.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        Text("Profile for: \(draftProfile.username)")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
