8e4f46b3ff657a8756f9df88cde910357e1a8560
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-28 15:15:55 +0530

    - Add UIPageViewControllerDelegate to track the current page on PageView

--- Landmarks-SwiftUI/Views/PageView/PageViewController.swift
+++ Landmarks-SwiftUI/Views/PageView/PageViewController.swift
@@ -20,7 +20,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
         pageViewController.dataSource = context.coordinator
-        
+        pageViewController.delegate = context.coordinator
         return pageViewController
     }
     
@@ -28,7 +28,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
         uiViewController.setViewControllers([context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers: [UIViewController] = []
         
@@ -55,6 +55,12 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
                 return controllers.first
             }
             return controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed, let visibleViewController = pageViewController.viewControllers?.first, let index = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
         }
     }
 }
