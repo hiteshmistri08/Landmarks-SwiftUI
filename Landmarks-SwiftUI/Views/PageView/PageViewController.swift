f349a2a0a6549937d59f18ec3bc99fe27e46252c
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-28 14:48:16 +0530

    - Add UIPageViewControllerDataSource to coordinator to load the pages of pageViewController

--- Landmarks-SwiftUI/Views/PageView/PageViewController.swift
+++ Landmarks-SwiftUI/Views/PageView/PageViewController.swift
@@ -18,6 +18,8 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
+        
         return pageViewController
     }
     
@@ -25,13 +27,33 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
         uiViewController.setViewControllers([context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
         var parent: PageViewController
         var controllers: [UIViewController] = []
         
         init(parent: PageViewController) {
             self.parent = parent
             controllers = parent.pages.map{ UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
         }
     }
 }
