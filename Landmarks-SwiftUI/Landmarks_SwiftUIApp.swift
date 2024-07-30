8c8b7121911f09dbb259a8a8c124f4b1d7186680
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-30 13:15:55 +0530

    - Add Notification Scene to landmark app for open the Notification View

--- Landmarks-SwiftUI/Landmarks_SwiftUIApp.swift
+++ Landmarks-SwiftUI/Landmarks_SwiftUIApp.swift
@@ -16,5 +16,9 @@ struct Landmarks_SwiftUIApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
