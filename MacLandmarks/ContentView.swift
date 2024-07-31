2216f80f1b7be486fc07619830725b1e3ca5931a
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-31 15:39:14 +0530

    - Add landmark list items in content view of mac os app

--- MacLandmarks/ContentView.swift
+++ MacLandmarks/ContentView.swift
@@ -16,11 +16,16 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        
+//        LandmarkList()
+//            .frame(minWidth: 700, minHeight: 300)
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
