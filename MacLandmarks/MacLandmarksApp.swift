b01b4ee640862a76b9c01ada9d19e59f0ab2506e
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-07-31 15:19:01 +0530

    - Create Mac OS Target and build the app

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Hitesh on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
