c48264043a1bbaf1ab1d7f7fd1a006623f4749d9
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-08-04 11:42:32 +0530

    - Add Command Menu with button Mark as Favorite and Remove Favorite functionality - Add short cut key "f" for mac os user to access easily

--- MacLandmarks/LandmarkCommands.swift
+++ MacLandmarks/LandmarkCommands.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct LandmarkCommands: Commands {
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
     var body: some Commands {
         SidebarCommands()
+        
+        CommandMenu("Landmark") {
+            Button((selectedLandmark?.isFavorite == true ? "Remove" : "Mark") + " as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
     }
 }
 
