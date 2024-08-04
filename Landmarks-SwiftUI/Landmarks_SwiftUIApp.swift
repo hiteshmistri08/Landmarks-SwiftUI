9ab7283754956d9145b2df46684413089c984830
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-08-04 11:23:00 +0530

    - LandmarkCommands only use for macOS and iOS app not for watchOS app

--- Landmarks-SwiftUI/Landmarks_SwiftUIApp.swift
+++ Landmarks-SwiftUI/Landmarks_SwiftUIApp.swift
@@ -16,9 +16,11 @@ struct Landmarks_SwiftUIApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
+        #if os(watchOS)
         .commands {
             LandmarkCommands()
         }
+        #endif
         
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
